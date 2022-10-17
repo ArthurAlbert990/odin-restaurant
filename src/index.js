bd6851ffe03bf010098fe94986f41304f72eb533
Arthur <arthur.albert990@gmail.com>
2022-10-17 14:46:21 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('js working');
+import css from "./styles.css";
+
+console.log('js working!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules'), 'node_modules']
+  },
+  module: {
+  rules:[{
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"],
+  }],}
 };
